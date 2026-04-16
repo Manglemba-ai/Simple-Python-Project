1da97bcb645e7769ca1609ac3ec45c39a1f4da47
Manglemba-ai <manglembaheikrujam552@gmail.com>
2026-04-16 11:56:51 +0530

    WIP: added task input and validation

--- project_1.py
+++ project_1.py
@@ -1,30 +1,48 @@
 
-def add_expences():
+def add_expense():
    amount=input("Enter the amount :")
    category = input("Enter your category :")
    date = input("Enter the date :")
 
-   with open("expences.txt","a") as file:
+   with open("D:\coding\python\expenses.txt","a") as file:
       file.write(f"{amount},{category},{date} \n")
 
 
-def view_expences():
+def view_expenses():
    try:
-      with open("expences.txt","a") as file:
+      with open("D:\coding\python\expenses.txt","a") as file:
          for line in file:
             amount,category,date =line.strip().split(",")
             print(amount,"|",category,"|",date)
    except FileNotFoundError:
-      print("No Expences Found.")
+      print("No Expenses Found.")
 
-def total__expences():
+def total__expense():
    total = 0
    try:
-      with open("expences.txt", "r") as file:
+      with open("D:\coding\python\expenses.txt", "r") as file:
          for line in file:
             amount,_, _ =line.strip().split(",")
             total += float(amount)
             print("Total spending :",total)
    except FileNotFoundError:
       print("No expenses found.")
-               
+while True:
+   print("\n1. Add Expenes")
+   print("2. View Expenes")
+   print("3. Show Total")
+   print("4.Exit")
+
+   choice = input("Choose: ")
+   if choice == "1":
+      add_expense()
+   elif choice == "2":
+      view_expenses()
+   elif choice == "3":
+      total__expense()
+   elif choice == "4":
+      break
+   else:
+      print("Invalid choice")
+                    
+
