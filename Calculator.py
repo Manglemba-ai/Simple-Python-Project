c6822cb152d737d5fe688d18a83a732339f26f3e
Manglemba-ai <manglembaheikrujam552@gmail.com>
2026-04-16 23:04:22 +0530

    added loop

--- Calculator.py
+++ Calculator.py
@@ -1,28 +1,35 @@
 # SIMPLE CALCULATOR
 
 def calculator():
-    try:
-        first = float(input("Enter first number :"))
-        second = float(input("Enter second number :"))
-    except ValueError:
-        print("Please enter valid number")
-        return
-    op = input("Enter operator(+,-,*,/) :")
-    if len(op) != 1 or op  not in "+-*/":
-        print("please enter valid operator")
-        return
-    if op == "+":
-        result = first+second
-    elif op == "-":
-        result = first-second
-    elif op == "*":
-        result = first*second
-    elif op == "/":
-        if second == 0:
-            print(f"can't divide by 0")
-            return
-        result = first/second
-    print(f"{first} {op} {second} = {result}")
+    while True:
+        try:
+            first = float(input("Enter first number :"))
+            second = float(input("Enter second number :"))
+        except ValueError:
+            print("Please enter valid number")
+            continue
+        op = input("Enter operator(+,-,*,/) :")
+        if len(op) != 1 or op not in "+-*/":
+            print("please enter valid operator")
+            continue
+        if op == "+":
+            result = first+second
+        elif op == "-":
+            result = first-second
+        elif op == "*":
+            result = first*second
+        elif op == "/":
+            if second == 0:
+                print(f"can't divide by 0")
+                continue
+            result = first/second
+        print(f"{first} {op} {second} = {result}")
+        again = input("do you want to continue (yes/no) :").strip().lower()
+        if again in ["yes", "y"]:
+            continue
+        elif again in ["no", "n"]:
+            print("Thank you")
+            break
 
 
 calculator()
