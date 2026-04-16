f6ad6ee5c2096887d0a857b17f919d28d5097a13
Manglemba-ai <manglembaheikrujam552@gmail.com>
2026-04-16 17:14:24 +0530

    fixed loophole

--- project_2.py
+++ project_2.py
@@ -1,31 +1,28 @@
-##SIMPLE CALCULATOR
+# SIMPLE CALCULATOR
 
 def calculator():
     try:
-        first_number = float(input("Enter first number :"))
-        operator= input("Enter operator :") 
-        second_number= float(input("Enter second number :"))
-    except ValueError:        
-        print("invalid")
+        first = float(input("Enter first number :"))
+        second = float(input("Enter second number :"))
+    except ValueError:
+        print("Please enter valid number")
         return
-    if operator == "+":
-        total_sum=first_number+second_number
-        print(f"{first_number} {operator} {second_number} = {total_sum}")
-    elif operator == "-":
-         total_subtraction=first_number-second_number
-         print(f"{first_number} {operator} {second_number} = {total_subtraction}")
-    elif operator == "*":
-        total_multiplication=first_number*second_number
-        print(f"{first_number} {operator} {second_number} = {total_multiplication}")
-    elif operator == "/":
-        if second_number == 0:
+    op = input("Enter operator(+,-,*,/) :")
+    if len(op) != 1 or op  not in "+-*/":
+        print("please enter valid operator")
+        return
+    if op == "+":
+        result = first+second
+    elif op == "-":
+        result = first-second
+    elif op == "*":
+        result = first*second
+    elif op == "/":
+        if second == 0:
             print(f"can't divide by 0")
-        else:
-            total_division=first_number/second_number 
-            print(f"{first_number} {operator} {second_number} = {total_division}")    
-    else:
-        print("invalid operator")            
+            return
+        result = first/second
+    print(f"{first} {op} {second} = {result}")
 
 
-    
 calculator()
