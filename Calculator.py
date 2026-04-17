0aa3ad320bd5eb1085a82884c6bb0ccd7046bf6d
Manglemba-ai <manglembaheikrujam552@gmail.com>
2026-04-17 21:49:30 +0530

    Updated with new feature

--- Calculator.py
+++ Calculator.py
@@ -24,36 +24,56 @@ def calculator():
                 continue
             result = first/second
         print(f"{first} {op} {second} = {result}")
-        history=input("Do you want to save history(yes/no) :").strip().lower()
-        if history in ["yes","y"]:
-            with open("History.txt","a") as file:
-                file.write(f"{first} {op} {second} = {result} \n")
-        elif history in ["no","n"]:
-            print("the history is not saved")
-        again = input("do you want to continue (yes/no) :").strip().lower()
-        if again in ["yes", "y"]:
-            continue
-        elif again in ["no", "n"]:    
+        while True:
+            history = input(
+                "Do you want to save history(yes/no) :").strip().lower()
+            if history in ["yes", "y", "no", "n"]:
+                break
+            print("Invalid input, try again.")
+        if history in ["yes", "y"]:
+            with open("History.txt", "a") as file:
+                file.write(f"{first} {op} {second} = {result}\n")
+        while True:
+            view_history_choice = input(
+                "Do you want to see the history(yes/no):").strip().lower()
+            if view_history_choice in ["yes", "y", "no", "n"]:
+                break
+            print("Invalid input, try again.")
+        if view_history_choice in ["yes", "y"]:
+            view_history()
+        while True:
+            delete_choice = input(
+                "do you want to delete history(yes/no) :").strip().lower()
+            if delete_choice in ["yes", "y", "no", "n"]:
+                break
+            print("Invalid input, try again.")
+        if delete_choice in ["yes", "y"]:
+            clear_history()
+        while True:
+            again = input("do you want to continue (yes/no) :").strip().lower()
+            if again in ["yes", "y","no", "n"]:
+                break
+            print("Invalid input, try again.")
+        if again in ["no", "n"]:
             break
+
 
 def view_history():
     try:
-        with open("History.txt","r")as file:
+        with open("History.txt", "r")as file:
             for line in file:
-                print(line)
+                print(line.strip())
     except FileNotFoundError:
         print("File not found")
 
+
+def clear_history():
+    Removed = input(
+        "Do you really want to delete history(yes/no):").strip().lower()
+    if Removed in ["yes", "y"]:
+        with open("History.txt", "w") as file:
+            file.write("")
+        print("File cleared")
+
+
 calculator()
-History = input("Do you want to see the history(yes/no):")
-if History in ["yes","y"]:
-    view_history()
-elif History in ["no","n"]:
-    print("Thank you ")          
-
-    
-
-
-
-
-
